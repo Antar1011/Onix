034daf4f2f063a45ef54c0a275ac53ba901efcee
Antar1011 <summer.kenster@gmail.com>
2016-05-29 13:47:50 -0400

    Functionality to pull PS data and convert it to JSON

---
+++ smogonusage/scrapers.py
@@ -0,0 +1,107 @@
+"""Functionality for pulling information from PS and converting to JSON"""
+
+from six.moves.urllib.request import urlopen
+import json
+
+import js2py
+
+
+def _scrape(url, entry, destination_filename=None):
+    """
+    Pulls javascript from the specified URL, extracts the requested entry
+    and returns it as a JSON string. Optionally writes said JSON string to
+    the requested file
+
+    Args:
+        url (str): the location of the javascript file on the PS Github
+            (url following ``master/``) or the full url to the file
+        entry (str): the ``exports`` entry we seek to extract
+        destination_filename (Optional(str)): if specified, the JSON string
+            will be written to this file
+
+    Returns:
+        str: the JSON string representation of the requested data
+
+    """
+    url_prefix = \
+        "https://raw.githubusercontent.com/Zarel/Pokemon-Showdown/master/"
+    if not url.startswith('https'):
+        url = url_prefix + url
+    prerun = 'exports={}; '
+    postrun = 'JSON.stringify(exports.'+entry+', null, 2)'
+    javascript = urlopen(url).read().decode('utf-8')
+
+    json_string = js2py.eval_js(prerun+javascript+postrun)
+
+    if destination_filename:
+        with open(destination_filename, 'w+') as out_file:
+            out_file.write(json_string)
+
+    return json_string
+
+
+def scrape_formats():
+    """
+    Grabs rulesets for the various metagames and saves it as formats.json.
+    Useful for extracting, say, banlists for non-standard tiers.
+
+    Returns:
+        list(dict): the data encoded in formats.js (each entry in the list
+            is a different metagame)
+
+    Examples:
+        >>> from smogonusage import scrapers
+        >>> formats = scrapers.scrape_formats()
+        >>> for metagame in formats:
+        ...    if metagame['name'] == 'LC':
+        ...        print(metagame['maxLevel'])
+        ...        break
+        ...
+        5
+    """
+    url = 'config/formats.js'
+    entry = 'Formats'
+    filename = 'resources/formats.json'
+    return json.loads(_scrape(url, entry, filename))
+
+
+def scrape_battle_formats_data():
+    """
+    Grabs data including tier information for Pokemon. Useful for extracting
+    banlists for the standard tiers.
+
+    Returns:
+        dict: the data encoded in formats-data.js. The keys are the species
+            names
+
+    Examples:
+        >>> from smogonusage import scrapers
+        >>> battle_formats = scrapers.scrape_battle_formats_data()
+        >>> print(battle_formats['bulbasaur']['tier'])
+        LC
+    """
+    url = 'data/formats-data.js'
+    entry = 'BattleFormatsData'
+    filename = 'resources/formats-data.json'
+    return json.loads(_scrape(url, entry, filename))
+
+
+def scrape_battle_pokedex():
+    """
+    Grabs data including base stats, types, and appearance-only form info.
+
+    Returns:
+        dict: the data encoded in pokedex.js. The keys are the species
+            names
+
+    Examples:
+        >>> from smogonusage import scrapers
+        >>> pokedex = scrapers.scrape_battle_pokedex()
+        >>> print(pokedex['bulbasaur']['baseStats']['hp'])
+        45
+    """
+
+    url = 'data/pokedex.js'
+    entry = 'BattlePokedex'
+    filename = 'resources/pokedex.json'
+    return json.loads(_scrape(url, entry, filename))
---
+++ smogonusage/tests/test_scrapers.py
@@ -0,0 +1,31 @@
+"""Tests for scrapers module"""
+import json
+
+from smogonusage import scrapers
+
+
+def test_scrape_formats():
+    formats = scrapers.scrape_formats()
+    formats_from_file = json.load(open('resources/formats.json'))
+    assert(formats == formats_from_file)
+    for metagame in formats:
+        if metagame['name'] == 'OU':
+            ou = metagame
+            break
+    assert('Uber' in metagame['banlist'])
+
+
+def test_scrape_battle_formats_data():
+    battle_formats = scrapers.scrape_battle_formats_data()
+    battle_formats_from_file = json.load(
+        open('resources/formats-data.json'))
+    assert(battle_formats == battle_formats_from_file)
+    assert(battle_formats['arceus']['tier'] == 'Uber')
+
+
+def test_scrape_battle_pokedex():
+    pokedex = scrapers.scrape_battle_pokedex()
+    pokedex_from_file = json.load(
+        open('resources/pokedex.json'))
+    assert(pokedex == pokedex_from_file)
+    assert(pokedex['sudowoodo']['types'] == ['Rock'])
