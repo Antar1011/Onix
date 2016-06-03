16ca916a2ac1a8ef9247bcf592266a28d1d8da43
Antar1011 <summer.kenster@gmail.com>
2016-06-02 23:18:41 -0400

    Started work on LogReader

--- smogonusage/log_reader.py
+++ smogonusage/log_reader.py
@@ -1,2 +1,32 @@
-"""Functionality for taking Pokemon Showdown logs and structuring them into a desired format"""
+"""Functionality for taking PS logs & structuring them into a desired format"""
 
+import abc
+
+from smogonusage.moveset import Moveset
+
+
+class LogReader(object):
+    """
+    An object which takes in a Pokemon Showdown log (in whatever format it
+    exists) and returned structured data, ready for compiling or storing
+    """
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def parse_log(self, log):
+        """
+        Parse the provided log and return structured
+        Args:
+            log: log of the  battle, in whatever format the reader can parse
+
+        Returns:
+            BattleInfo: metadata about the match
+            tuple(tuple(Moveset)): the movesets used in the battle, grouped by
+                team
+            list(Matchup): list of matchups
+
+        Raises:
+            ParsingException: if there's a problem parsing the log
+
+        """
+        pass
