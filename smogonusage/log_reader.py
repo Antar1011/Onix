887e82a2ea142977c0fdc1a5da0581ebbc8e67a9
Antar1011 <summer.kenster@gmail.com>
2016-06-03 23:37:59 -0400

    Started work on log reader for PS .jsons

--- smogonusage/log_reader.py
+++ smogonusage/log_reader.py
@@ -1,8 +1,11 @@
 """Functionality for taking PS logs & structuring them into a desired format"""
 
 import abc
+import json
 
-from smogonusage.moveset import Moveset
+from smogonusage.dto import *
+from smogonusage import scrapers
+from smogonusage import utilities
 
 
 class LogReader(object):
@@ -15,18 +18,77 @@ class LogReader(object):
     @abc.abstractmethod
     def parse_log(self, log):
         """
-        Parse the provided log and return structured
+        Parse the provided log and return structured data
+
         Args:
-            log: log of the  battle, in whatever format the reader can parse
+            log: the log to parse, in whatever format the reader can parse
 
         Returns:
-            BattleInfo: metadata about the match
-            tuple(tuple(Moveset)): the movesets used in the battle, grouped by
-                team
-            list(Matchup): list of matchups
+            (tuple):
+                * BattleInfo: metadata about the match
+                * tuple(tuple(Moveset)): the movesets used in the battle,
+                    grouped by team
+                * list(Matchup): list of matchups
 
         Raises:
             ParsingException: if there's a problem parsing the log
 
         """
         pass
+
+
+class JsonFileLogReader(LogReader):
+
+    def __init__(self, sanitizer=None, pokedex=None):
+        """
+        Parses Pokemon Showdown ``.json`` files
+
+        Args:
+            sanitizer (Optional(Sanitizer)):
+                Used to normalize the data read in from the log. If none is
+                    specified, will create its own
+            pokedex (Optional(dict)):
+                Used to calculate base stats. If none is specified will attempt
+                to load from file, and if the file doesn't exist will scrape it
+                from the Pokemon Showdown github
+        """
+        if sanitizer is None:
+            sanitizer = utilities.Sanitizer()
+        if pokedex is None:
+            try:
+                pokedex = json.load(open('resources/pokedex.json'))
+            except FileNotFoundError:
+                pokedex = scrapers.scrape_battle_pokedex()
+
+        self.sanitizer = sanitizer
+        self.base_stats = dict()
+        for pokemon in pokedex.keys():
+            self.base_stats[pokemon] = utilities.stats_dict_to_dto(
+                pokedex[pokemon]['baseStats'])
+
+    def parse_log(self, log):
+        """
+        Parse the provided log and return structured data
+
+        Args:
+            log (str): file name of the battle log to parse
+
+        Returns:
+            (tuple):
+                * BattleInfo: metadata about the match
+                * tuple(tuple(Moveset)): the movesets used in the battle,
+                    grouped by team
+                * list(Matchup): list of matchups
+
+        Raises:
+            ParsingException: if there's a problem parsing the log
+            FileNotFoundError: if the log doesn't exist
+            json.decoder.JSONDecodeError: if the log is not a valid log
+
+        """
+
+        log = json.load(open(log))
+        pass
+
+
+
