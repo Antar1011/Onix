49dd460f10c70d6d8f6ba488cdff8885d2025e0b
Antar1011 <summer.kenster@gmail.com>
2016-08-26 15:14:15 -0400

    Cleaned up reporting tests

--- onix/tests/reporting/test_reports.py
+++ onix/tests/reporting/test_reports.py
@@ -55,6 +55,14 @@ class TestGenerateUsageStats(object):
                                          baseline=1695.0,
                                          unkown_species_handling='raise')
 
+    def test_raise_error_if_invalid_error_handling_kwarg(self):
+        with pytest.raises(ValueError):
+            reports.generate_usage_stats(dao,
+                                         self.lookup,
+                                         '2016-08', 'ou',
+                                         baseline=1695.0,
+                                         unkown_species_handling='afadgadg')
+
     def test_dao_calls(self):
         with pytest.raises(TypeError) as e:
             reports.generate_usage_stats(dao,
@@ -62,24 +70,24 @@ class TestGenerateUsageStats(object):
                                          '2016-08', 'adgadgad')
         assert str(e.value).startswith("'NoneType' object")
 
-    def test_generate_ou_report_guess_un(self):
+    def test_generate_ou_report_guess_unknown_species(self):
 
         expected = "Total battles: 5000\n" \
                    "Avg. weight / team: 0.653408\n" \
                    "+ ---- + ------------------------- + --------- +\n" \
                    "| Rank | Species                   | Usage %   |\n" \
                    "+ ---- + ------------------------- + --------- +\n" \
-                   "|    1 | Landorus-Therian          |  94.5516% |\n" \
-                   "|    2 | Heatran                   |  92.8308% |\n" \
-                   "|    3 | Latios                    |  87.3387% |\n" \
-                   "|    4 | Garchomp                  |  86.3952% |\n" \
-                   "|    5 | Charizard-Mega-X          |  60.6806% |\n" \
-                   "|    6 | Charizard-Mega-Y          |  58.8796% |\n" \
-                   "|    7 | Scizor-Mega               |  54.9298% |\n" \
-                   "|    8 | Scizor                    |  48.6641% |\n" \
-                   "|    9 | Gastrodon                 |   9.3815% |\n" \
+                   "|    1 | Landorus-Therian          |  94.5475% |\n" \
+                   "|    2 | Heatran                   |  92.8267% |\n" \
+                   "|    3 | Latios                    |  87.3349% |\n" \
+                   "|    4 | Garchomp                  |  86.3919% |\n" \
+                   "|    5 | Charizard-Mega-X          |  60.6780% |\n" \
+                   "|    6 | Charizard-Mega-Y          |  58.8770% |\n" \
+                   "|    7 | Scizor-Mega               |  54.9274% |\n" \
+                   "|    8 | Scizor                    |  48.6619% |\n" \
+                   "|    9 | Gastrodon                 |   9.3812% |\n" \
                    "|   10 | Charizard                 |   4.4586% |\n" \
-                   "|   11 | Froobat                   |   4.4586% |\n" \
+                   "|   11 | Froobat                   |   0.0263% |\n" \
                    "+ ---- + ------------------------- + --------- +\n"
 
         output = reports.generate_usage_stats(dao,
