a64a70aa3ea41e80d39b0005acbb0be06e0935df
Antar1011 <summer.kenster@gmail.com>
2016-08-26 17:01:50 -0400

    generate_usage_stats is now written and tested!

--- onix/reporting/reports.py
+++ onix/reporting/reports.py
@@ -1,16 +1,18 @@
 """Methods for generating various reports"""
+from collections import Counter
+
 import six
 
 from onix.reporting import dao
 
 
-def generate_usage_stats(dao, species_lookup, month, metagame,
+def generate_usage_stats(reporting_dao, species_lookup, month, metagame,
                          baseline=1630.0, unknown_species_handling='raise'):
     """
     Generate a usage stats report
 
     Args:
-        dao (dao) :
+        reporting_dao (dao.ReportingDAO) :
             access object used to grab usage data
         species_lookup (dict) :
             mapping of species names or forme-concatenations to their display
@@ -36,3 +38,65 @@ def generate_usage_stats(dao, species_lookup, month, metagame,
             the usage stats report, ready for printing to stdout or saving to
             file
     """
+
+    n_battles = reporting_dao.get_number_of_battles(month, metagame)
+
+    usage_data = reporting_dao.get_usage_by_species(month, metagame, baseline)
+
+    total_usage = sum(usage_data.values())/6
+
+    pct_usage = Counter()
+
+    for species, value in six.iteritems(usage_data):
+
+        if species is None:  # skip "empty" slots
+            continue
+        elif species in species_lookup.keys():
+            pretty_species = species_lookup[species]
+        else:
+            if unknown_species_handling == 'raise':
+                raise KeyError('Unknown species or forme-concatenation: {0}'
+                               .format(species))
+            elif unknown_species_handling == 'guess':
+                pretty_species = species.title()
+            else:
+                raise ValueError('Invalid mode for unknown_species_handling: '
+                                 '{0}'.format(unknown_species_handling))
+
+        pct_usage[pretty_species] += 100.0*value/total_usage
+
+    sorted_usage = sorted([(species, usage) for species, usage
+                           in six.iteritems(pct_usage)], key=lambda x:-x[1])
+
+    longest_species_length = max(map(lambda x:len(x), pct_usage))
+
+    column_widths = {'rank': 4,
+                     'species': max(25, longest_species_length),
+                     'usage': 9}
+
+    report_lines = [' Total battles: {0:d}'.format(n_battles),
+                    ' Avg. weight / team: {0:8f}'.format(
+                        total_usage / n_battles / 2)]
+
+    separator = ''
+    for key in ('rank', 'species', 'usage'):
+        separator += ' + ' + '-'*column_widths[key]
+    separator += ' +'
+
+    report_lines.append(separator)
+    report_lines.append(' | {0: <{1}} | {2: <{3}} | {4: <{5}} |'.format(
+        'Rank', column_widths['rank'],
+        'Species', column_widths['species'],
+        'Usage %', column_widths['usage']))
+    report_lines.append(separator)
+
+    for i in range(len(sorted_usage)):
+        report_lines.append(' | {0:{1}d} | {2: <{3}} | {4:{5}.{6}f}% |'.format(
+            i+1, column_widths['rank'],
+            sorted_usage[i][0], column_widths['species'],
+            sorted_usage[i][1],
+            column_widths['usage']-1, column_widths['usage']-5))
+
+    report_lines.append(separator)
+
+    return '\n'.join(report_lines)+'\n'
--- onix/tests/reporting/test_reports.py
+++ onix/tests/reporting/test_reports.py
@@ -11,7 +11,7 @@ from onix.reporting import reports
 class MockReportingDao(dao.ReportingDAO):
 
     def get_usage_by_species(self, month, metagame, baseline=1630.0):
-        if metagame == 'ou':
+        if metagame == 'ou' and month == '2016-08' and baseline == 1695.0:
             return {'landorustherian': 6178.08,
                     'heatran': 6065.64,
                     'garchomp': 5645.13,
@@ -25,10 +25,11 @@ class MockReportingDao(dao.ReportingDAO):
                     'gastrodoneast': 169.59,
                     'froobat': 1.72,
                     None: 123.45}
+        elif metagame == 'superlongspeciesname':
+            return {'iamtheverymodelofamodernmajorgeneral': 100.0}
 
     def get_number_of_battles(self, month, metagame):
-        if metagame == 'ou':
-            return 5000
+        return 5000
 
 
 class TestGenerateUsageStats(object):
@@ -45,56 +46,93 @@ class TestGenerateUsageStats(object):
                        'charizard,charizardmegax': 'Charizard-Mega-X',
                        'charizard,charizardmegay': 'Charizard-Mega-Y',
                        'gastrodon': 'Gastrodon',
-                       'gastrodoneast': 'Gastrodon_East',
+                       'gastrodoneast': 'Gastrodon',
                        'quilava': 'Quilava'}
 
     def test_raise_error_for_unkown_species(self):
         with pytest.raises(KeyError):
-            reports.generate_usage_stats(dao,
+            reports.generate_usage_stats(self.dao,
                                          self.lookup,
                                          '2016-08', 'ou',
                                          baseline=1695.0,
-                                         unkown_species_handling='raise')
+                                         unknown_species_handling='raise')
+
+        with pytest.raises(KeyError):
+            reports.generate_usage_stats(self.dao,
+                                         self.lookup,
+                                         '2016-08', 'ou',
+                                         baseline=1695.0)
 
     def test_raise_error_if_invalid_error_handling_kwarg(self):
         with pytest.raises(ValueError):
-            reports.generate_usage_stats(dao,
+            reports.generate_usage_stats(self.dao,
                                          self.lookup,
                                          '2016-08', 'ou',
                                          baseline=1695.0,
-                                         unkown_species_handling='afadgadg')
+                                         unknown_species_handling='afadgadg')
 
     def test_dao_calls(self):
-        with pytest.raises(TypeError) as e:
-            reports.generate_usage_stats(dao,
+        with pytest.raises(AttributeError) as e:
+            reports.generate_usage_stats(self.dao,
                                          self.lookup,
-                                         '2016-08', 'adgadgad')
+                                         '2016-08', 'adgadgad',
+                                         baseline=1695.0)
+        assert str(e.value).startswith("'NoneType' object")
+
+        with pytest.raises(AttributeError) as e:
+            reports.generate_usage_stats(self.dao,
+                                         self.lookup,
+                                         '2016-09', 'ou',
+                                         baseline=1695.0)
+        assert str(e.value).startswith("'NoneType' object")
+
+        with pytest.raises(AttributeError) as e:
+            reports.generate_usage_stats(self.dao,
+                                         self.lookup,
+                                         '2016-08', 'ou')
         assert str(e.value).startswith("'NoneType' object")
 
     def test_generate_ou_report_guess_unknown_species(self):
 
-        expected = "Total battles: 5000\n" \
-                   "Avg. weight / team: 0.653408\n" \
-                   "+ ---- + ------------------------- + --------- +\n" \
-                   "| Rank | Species                   | Usage %   |\n" \
-                   "+ ---- + ------------------------- + --------- +\n" \
-                   "|    1 | Landorus-Therian          |  94.5475% |\n" \
-                   "|    2 | Heatran                   |  92.8267% |\n" \
-                   "|    3 | Latios                    |  87.3349% |\n" \
-                   "|    4 | Garchomp                  |  86.3919% |\n" \
-                   "|    5 | Charizard-Mega-X          |  60.6780% |\n" \
-                   "|    6 | Charizard-Mega-Y          |  58.8770% |\n" \
-                   "|    7 | Scizor-Mega               |  54.9274% |\n" \
-                   "|    8 | Scizor                    |  48.6619% |\n" \
-                   "|    9 | Gastrodon                 |   9.3812% |\n" \
-                   "|   10 | Charizard                 |   4.4586% |\n" \
-                   "|   11 | Froobat                   |   0.0263% |\n" \
-                   "+ ---- + ------------------------- + --------- +\n"
+        expected = " Total battles: 5000\n" \
+                   " Avg. weight / team: 0.653437\n" \
+                   " + ---- + ------------------------- + --------- +\n" \
+                   " | Rank | Species                   | Usage %   |\n" \
+                   " + ---- + ------------------------- + --------- +\n" \
+                   " |    1 | Landorus-Therian          |  94.5475% |\n" \
+                   " |    2 | Heatran                   |  92.8267% |\n" \
+                   " |    3 | Latios                    |  87.3349% |\n" \
+                   " |    4 | Garchomp                  |  86.3914% |\n" \
+                   " |    5 | Charizard-Mega-X          |  60.6780% |\n" \
+                   " |    6 | Charizard-Mega-Y          |  58.8770% |\n" \
+                   " |    7 | Scizor-Mega               |  54.9274% |\n" \
+                   " |    8 | Scizor                    |  48.6619% |\n" \
+                   " |    9 | Gastrodon                 |   9.3812% |\n" \
+                   " |   10 | Charizard                 |   4.4584% |\n" \
+                   " |   11 | Froobat                   |   0.0263% |\n" \
+                   " + ---- + ------------------------- + --------- +\n"
 
-        output = reports.generate_usage_stats(dao,
+        output = reports.generate_usage_stats(self.dao,
                                               self.lookup,
                                               '2016-08', 'ou',
                                               baseline=1695.0,
                                               unknown_species_handling='guess')
         assert expected == output
 
+    def test_long_species_report(self):
+
+        exp = " Total battles: 5000\n" \
+              " Avg. weight / team: 0.001667\n" \
+              " + ---- + ------------------------------------ + --------- +\n" \
+              " | Rank | Species                              | Usage %   |\n" \
+              " + ---- + ------------------------------------ + --------- +\n" \
+              " |    1 | Iamtheverymodelofamodernmajorgeneral | 600.0000% |\n" \
+              " + ---- + ------------------------------------ + --------- +\n"
+
+        output = reports.generate_usage_stats(self.dao,
+                                              self.lookup,
+                                              '2016-08',
+                                              'superlongspeciesname',
+                                              unknown_species_handling='guess')
+
+        assert exp == output
