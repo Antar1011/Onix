97726cb0564f2a3e7dcb1d753d82864e8355de2f
Antar1011 <summer.kenster@gmail.com>
2016-09-09 08:32:35 -0400

    compute_fid utility
    For now I think it's valid to treat this as SQL-specific. Use-cases where that degree of normalization isn't required won't need it.

---
+++ onix/backends/sqlite/sinks.py
@@ -0,0 +1,33 @@
+"""Sink implementations for SQLite backend"""
+import hashlib
+
+from onix import dto
+from onix import utilities
+from onix.backends.sqlite import declarative
+
+
+def compute_fid(forme):
+    """
+    Computes the Forme ID for a given forme
+
+    Args:
+        forme (dto.Forme) : the forme to compute the FID for. Is assumed to be
+            sanitized.
+
+    Returns:
+        str : the corresponding Forme ID
+
+    Examples:
+        >>> from onix.dto import Forme, PokeStats
+        >>> from onix.backends.sqlite.sinks import compute_fid
+        >>> forme = Forme('stunfisk', 'static',
+        ...               PokeStats(369, 168, 177, 258, 225, 73))
+        >>> print(compute_fid(forme)) #doctest: +ELLIPSIS
+        tbd
+    """
+    forme_hash = hashlib.sha512(repr(forme).encode('utf-8')).hexdigest()
+
+    # may eventually want to truncate hash, e.g.
+    # forme_hash = forme_hash[:16]
+
+    return forme_hash
