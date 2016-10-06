6cddff43098c851070a44e3aed8868aac5927254
Antar1011 <summer.kenster@gmail.com>
2016-10-06 15:56:48 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import versioneer
+from setuptools import setup
+from onix import __author__
+
+setup(
+    name='Onix',
+    description='The Pokemon data mining package',
+    packages=['onix'],
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    url='https://github.com/Antar1011/Onix',
+    license='GPL v3',
+    author=__author__,
+    author_email='antar@smgoon.com',
+    install_requires=[
+        "future",
+        "py-mini-racer>=0.1.6",
+        "setuptools>=0.8",
+        "sqlalchemy>=1.0.0"
+    ]
+)
+
